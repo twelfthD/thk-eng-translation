f3ed2d0d199539a124bcfbe38900d0f263f8dda1
twelfthD <gsamuelkipgen@mail.com>
2022-06-05 16:53:04 +0530

    points

--- joinline.py
+++ joinline.py
@@ -3,6 +3,23 @@ with open("niv.txt", "r", encoding="utf8") as f:
 with open("niv.txt", "w") as f:
     for line in lines:
         #f.write(line.replace("\n", " "))
-        f.write(line.replace(" 9", "\n9"))
+        #f.write(line.replace(" 9", "\n9"))
+
+        # if line word count is less than 1, join to previous line
+        if len(line.split()) <= 1:
+            # join to previous line
+            lines[lines.index(line) - 1] += line
+        else:
+            f.write(line)
+            
+
+           
+
+        
 
 
+
+
+
+        
+        
