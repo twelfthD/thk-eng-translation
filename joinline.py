2bf2b7df627b0a13033bcec62135fc27975a9345
twelfthD <gsamuelkipgen@mail.com>
2022-06-08 23:15:58 +0530

    format for new line
    Verse wise creating a new line

--- joinline.py
+++ joinline.py
@@ -2,15 +2,20 @@ with open("niv.txt", "r", encoding="utf8") as f:
     lines = f.readlines()
 with open("niv.txt", "w") as f:
     for line in lines:
-        #f.write(line.replace("\n", " "))
-        #f.write(line.replace(" 9", "\n9"))
+        # # except for the first line, remove the newline character
+        # if line != lines[0]:
+        #     line = line.rstrip()
+        # f.write(line)
 
-        # if line word count is less than 1, join to previous line
-        if len(line.split()) <= 1:
-            # join to previous line
-            lines[lines.index(line) - 1] += line
-        else:
-            f.write(line)
+        for i in range(10): # add newline characters before the verse number
+            # find "."+str(i) in the line and replace it with ".\n"+ str(i)
+            line = line.replace('"{}'.format(i), '"\n{}'.format(i))
+        f.write(line)
+
+
+
+
+
             
 
            
