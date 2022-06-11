78f8f21e48ada7d83b46918f5a5ae61cdc8f4b4b
twelfthD <gsamuelkipgen@mail.com>
2022-06-11 13:30:16 +0530

    ok for once

--- joinline.py
+++ joinline.py
@@ -1,13 +1,16 @@
-with open("niv.txt", "r", encoding="utf8") as f:
+import re
+with open("niv2.txt", "r", encoding="utf8") as f:
     lines = f.readlines()
-with open("niv.txt", "w") as f:
-    for line in lines:
+with open("niv2.txt", "w") as f:
+
+
+    
         # # except for the first line, remove the newline character
         # if line != lines[0]:
         #     line = line.rstrip()
         # f.write(line)
-
-        for i in range(10): # add newline characters before the verse number
+    for line in lines:
+        for i in range(100, 177): # add newline characters before the verse number
             # find "."+str(i) in the line and replace it with ".\n"+ str(i)
             #line = line.replace(',{}'.format(i), ',\n{}'.format(i))
             # line = line.replace("'{}".format(i), "'\n{}".format(i))
@@ -15,9 +18,35 @@ with open("niv.txt", "w") as f:
             # line = line.replace("?{}".format(i), "?\n{}".format(i))
             # line = line.replace("!{}".format(i), "!\n{}".format(i))
             # line = line.replace(":{}".format(i), ":\n{}".format(i))
-            # line = line.replace(";{}".format(i), ";\n{}".format(i))
-            line = line.replace("{},\n".format(i), "{},".format(i))
-        f.write(line)
+            line = line.replace("{} ".format(i), "")
+
+            
+
+
+
+        # for i in range(1):       
+        #     for letter in range(65):
+        #         for line in lines:
+        #             toletter = str(chr(letter))
+        #             num = str(i)
+        #             line = line.replace("\n"+ num + toletter, "\n"+ num + " " + toletter)
+        #             f.write(line)
+
+        # # add space between words using regex "(?i)(?:(?<=\d)(?=[a-z])|(?<=[a-z])(?=\d))"g
+        # for line in lines:
+        #     line = re.sub(r'(?i)(?:(?<=\d)(?=[a-z])|(?<=[a-z])(?=\d))', ' ', line)
+        #     f.write(line)
+
+
+        
+
+    
+    
+            
+
+
+
+        
 
 
 
---
+++ test.py
@@ -0,0 +1,14 @@
+# prints all the alphabetical characters in the alphabet
+for number in range(10):
+    for letter in range(65, 91):
+        print("{}".format(number) + " " +chr(letter), end=" ")
+    print()
+
+    
+
+
+
+
+
+
+
