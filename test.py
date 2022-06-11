4f2a7063c81c9abbacdfa96e4c51b28afbc2553a
twelfthD <gsamuelkipgen@mail.com>
2022-06-11 13:57:48 +0530

    join eng and thk

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-# prints all the alphabetical characters in the alphabet
-for number in range(10):
-    for letter in range(65, 91):
-        print("{}".format(number) + " " +chr(letter), end=" ")
-    print()
+# join two files line by line
+combine =[]
+
+with open("niv.txt", encoding="utf8") as xh:
+  with open('bible_thk.txt', encoding="utf8") as yh:
+    with open("eng_thk1.txt","w" , encoding="utf8") as zh:
+      #Read first file
+      xlines = xh.readlines()
+      #Read second file
+      ylines = yh.readlines()
+      #Combine content of both lists
+      #combine = list(zip(ylines,xlines))
+      #Write to third file
+      for i in range(len(xlines)):
+        line = ylines[i].strip() + '@1' + xlines[i]
+        zh.write(line)
+
 
     
 
